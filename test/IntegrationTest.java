a18ed204cb465eb584ed6198431bf2f7df67c2ff
tourn <latzer.daniel@gmail.com>
2016-03-30 18:36:54 +0200

    Get Basic integration test to work
    IMPORTANT: you need either the Ebean weaver for IntelliJ or the java
    agent as seen in
    http://blog.matthieuguillermin.fr/2012/03/unit-testing-tricks-for-play-2-0-and-ebean/
    for the tests to work from IntelliJ

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 
+import org.junit.runner.RunWith;
 import play.mvc.*;
 import play.test.*;
 
@@ -10,16 +11,15 @@ import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class IntegrationTest {
 
-    /**
-     * add your integration test here
-     * in this example we just check if the welcome page is being shown
-     */
     @Test
     public void test() {
-//        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
-//            browser.goTo("http://localhost:3333");
-//            assertTrue(browser.pageSource().contains("Your new application is ready."));
-//        });
+        running(testServer(3333, fakeApplication()), HTMLUNIT, browser -> {
+            browser.goTo("http://localhost:3333/");
+            assertTrue(browser.pageSource().contains("Aufgaben"));
+            browser.fill("#username").with("Franz");
+            browser.submit("#btn_login");
+            assertEquals("Franz", browser.find("#lbl_username").getText());
+        });
     }
 
 }
