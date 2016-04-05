0baf4b5743759f04680255d78dfec68f68d3e69e
blubiszirp <frank-jordi@hotmail.com>
2016-04-05 11:46:02 +0200

    using PagedList for Pagination

--- app/controllers/ExerciseController.java
+++ app/controllers/ExerciseController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import com.avaje.ebean.PagedList;
 import repositories.ExerciseRepository;
 import models.Exercise;
 import play.data.Form;
@@ -25,14 +26,19 @@ public class ExerciseController extends Controller {
     @Inject
     ExerciseRepository exerciseRepository;
 
+    public Result renderOverview(){
+        return list(0,"title","");
+    }
+
     public Result index() {
-        return ok(exerciseList.render(0,exerciseRepository.find().all(),"title","",5,exerciseRepository.find().all().size()));
+        return list(0,"title","");
+        //return ok(exerciseList.render(0,exerciseRepository.find().all(),"title","",5,exerciseRepository.find().all().size()));
     }
 
     public Result list(int page, String orderBy, String filter){
         int pageSize = 5;
-        java.util.List<Exercise> exercises = exerciseRepository.find().where().contains("title",filter).orderBy(orderBy).findPagedList(page,pageSize).getList();
-        return ok(exerciseList.render(page,exercises,orderBy,filter,pageSize,exerciseRepository.find().all().size()));
+        PagedList<Exercise> exercises = exerciseRepository.find().where().contains("title", filter).orderBy(orderBy).findPagedList(page, pageSize);
+        return ok(exerciseList.render(exercises,orderBy,filter));
     }
 
     public Result edit(long id) {
