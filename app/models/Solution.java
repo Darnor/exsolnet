10c53fb970a36bd52d2f467747f09cfe2f8838a8
tourn <latzer.daniel@gmail.com>
2016-04-05 18:09:35 +0200

    Add TagRepositoryTest

--- app/models/Solution.java
+++ app/models/Solution.java
@@ -1,6 +1,7 @@
 package models;
 
 import javax.persistence.*;
+import java.sql.Date;
 import java.util.List;
 
 /**
@@ -34,5 +35,60 @@ public class Solution  extends  Post{
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+
+    public static class SolutionBuilder {
+        private User user;
+        private Long id;
+        private String content;
+        private Date time;
+        private int points;
+
+        private SolutionBuilder() {
+        }
+
+        public static SolutionBuilder aSolution() {
+            return new SolutionBuilder();
+        }
+
+        public SolutionBuilder withUser(User user) {
+            this.user = user;
+            return this;
+        }
+
+        public SolutionBuilder withId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public SolutionBuilder withContent(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public SolutionBuilder withTime(Date time) {
+            this.time = time;
+            return this;
+        }
+
+        public SolutionBuilder withPoints(int points) {
+            this.points = points;
+            return this;
+        }
+
+        public SolutionBuilder but() {
+            return aSolution().withUser(user).withId(id).withContent(content).withTime(time).withPoints(points);
+        }
+
+        public Solution build() {
+            Solution solution = new Solution();
+            solution.setUser(user);
+            solution.setId(id);
+            solution.setContent(content);
+            solution.setTime(time);
+            solution.setPoints(points);
+            return solution;
+        }
     }
 }
---
+++ test/repositories/TagRepositoryTest.java
@@ -0,0 +1,76 @@
+package repositories;
+
+import models.Exercise;
+import models.Solution;
+import models.Tag;
+import models.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static models.Exercise.ExerciseBuilder.anExercise;
+import static models.Solution.SolutionBuilder.aSolution;
+import static models.Tag.TagBuilder.aTag;
+import static models.User.UserBuilder.anUser;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by tourn on 5.4.16.
+ */
+public class TagRepositoryTest {
+
+    private TagRepository tagRepo;
+
+    @Before
+    public void setUp(){
+        tagRepo = new TagRepository();
+
+    }
+
+    @Test
+    public void testGetCompletedExercisesNoSolutions(){
+        User userA = anUser().withEmail("Hans").build();
+        Exercise e1 = anExercise().withTitle("Exercise 1").build();
+        Exercise e2 = anExercise().withTitle("Exercise 2").build();
+        Tag tag = aTag().withName("A").withExercises(Arrays.asList(e1, e2)).build();
+        assertEquals(0, tagRepo.getNofCompletedExercises(tag, userA));
+    }
+
+    @Test
+    public void testGetCompletedExercisesNoExercises(){
+        User userA = anUser().withEmail("Hans").build();
+        Tag tag = aTag().withName("A").withExercises(Arrays.asList()).build();
+        assertEquals(0, tagRepo.getNofCompletedExercises(tag, userA));
+    }
+
+    @Test
+    public void testGetCompletedExercisesOnlySolution(){
+        User userA = anUser().withEmail("Hans").build();
+        Solution s1 = aSolution().withUser(userA).build();
+        Solution s2 = aSolution().withUser(userA).build();
+        Exercise e1 = anExercise().withTitle("Exercise 1").withSolutions(Arrays.asList(s1)).build();
+        Exercise e2 = anExercise().withTitle("Exercise 2").withSolutions(Arrays.asList(s2)).build();
+        Exercise e3 = anExercise().withTitle("Exercise 3").withSolutions(Arrays.asList()).build();
+        Tag tag = aTag().withName("A").withExercises(Arrays.asList(e1, e2, e3)).build();
+        assertEquals(2, tagRepo.getNofCompletedExercises(tag, userA));
+    }
+
+    @Test
+    public void testGetCompletedExercisesSeveralSolutions(){
+        User userA = anUser().withEmail("Hans").build();
+        User userB = anUser().withEmail("Franz").build();
+        Solution s11 = aSolution().withUser(userA).build();
+        Solution s12 = aSolution().withUser(userB).build();
+        Solution s21 = aSolution().withUser(userA).build();
+        Solution s22 = aSolution().withUser(userB).build();
+        Solution s32 = aSolution().withUser(userB).build();
+        Exercise e1 = anExercise().withTitle("Exercise 1").withSolutions(Arrays.asList(s11, s12)).build();
+        Exercise e2 = anExercise().withTitle("Exercise 2").withSolutions(Arrays.asList(s21, s22)).build();
+        Exercise e3 = anExercise().withTitle("Exercise 3").withSolutions(Arrays.asList(s32)).build();
+        Tag tag = aTag().withName("A").withExercises(Arrays.asList(e1, e2, e3)).build();
+        assertEquals(2, tagRepo.getNofCompletedExercises(tag, userA));
+    }
+}
