0a41c3062592e531023530a3635d4003571eb2ed
tourn <latzer.daniel@gmail.com>
2016-04-06 08:26:57 +0200

    Uncomment but ignore sample tests

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,26 +1,23 @@
-//import org.junit.*;
-//
-//import org.junit.runner.RunWith;
-//import play.mvc.*;
-//import play.test.*;
-//
-//import static org.hamcrest.core.StringContains.containsString;
-//import static play.test.Helpers.*;
-//import static org.junit.Assert.*;
-//
-//import static org.fluentlenium.core.filter.FilterConstructor.*;
-//
-//public class IntegrationTest {
-//
-//    @Test
-//    public void test() {
-//        running(testServer(3333, fakeApplication()), FIREFOX, browser -> {
-//            browser.goTo("http://localhost:3333/");
-//            assertThat(browser.pageSource(), containsString("Aufgaben"));
-//            browser.fill("#username").with("Franz");
-//            browser.submit("#btn_login");
-//            assertEquals("Franz", browser.find("#lbl_username").getText());
-//        });
-//    }
-//
-//}
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static play.test.Helpers.*;
+
+@Ignore
+public class IntegrationTest {
+
+    @Test
+    public void test() {
+        running(testServer(3333, fakeApplication()), FIREFOX, browser -> {
+            browser.goTo("http://localhost:3333/");
+            assertThat(browser.pageSource(), containsString("Aufgaben"));
+            browser.fill("#username").with("Franz");
+            browser.submit("#btn_login");
+            assertEquals("Franz", browser.find("#lbl_username").getText());
+        });
+    }
+
+}
