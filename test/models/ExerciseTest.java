2e927a04ea6f2d7613b8ee2858b5613317afd700
blubiszirp <frank-jordi@hotmail.com>
2016-04-11 15:20:49 +0200

    add OrderBy-Filter test

--- test/models/ExerciseTest.java
+++ test/models/ExerciseTest.java
@@ -30,5 +30,19 @@ public class ExerciseTest extends AbstractModelTest {
         assertEquals("XXX", page.getList().get(0).getTitle());
         assertEquals("Unit Testing", page.getList().get(1).getTitle());
     }
+    @Test
+    public void testGetPagedOrderedByRatingDesc(){
+        PagedList<Exercise> page = Exercise.getPagedList(0, "points desc", "", new String[]{""}, 5);
+        assertEquals(5, page.getList().size());
+        assertEquals("Error Handling Design", page.getList().get(0).getTitle());
+        assertEquals("Basic Math", page.getList().get(1).getTitle());
+    }
 
+    @Test
+    public void testGetOrderByAttributeString(){
+        assertEquals("title",Exercise.getOrderByAttributeString(1));
+        assertEquals("solutionCount",Exercise.getOrderByAttributeString(2));
+        assertEquals("points",Exercise.getOrderByAttributeString(3));
+        assertEquals("time",Exercise.getOrderByAttributeString(4));
+    }
 }
