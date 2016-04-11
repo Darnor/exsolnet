71feac4f4b23803baa7ab4cab2701a96593e5690
gummibjorn <marioegli95@gmail.com>
2016-04-11 11:19:49 +0200

    update javadoc on authenticator

--- app/controllers/Secured.java
+++ app/controllers/Secured.java
@@ -10,10 +10,22 @@ public class Secured extends Security.Authenticator {
     @Inject
     SessionService sessionService;
 
+    /**
+     * takes the pending requests context and checks if a user is logged in,
+     * return User if logged in, else returns null
+     * @param ctx
+     * @return
+     */
     @Override
     public String getUsername(Context ctx) {
         return sessionService.getCurrentUserEmail();
     }
+
+    /**
+     * if not logged in, this method redirect to given route
+     * @param ctx
+     * @return
+     */
     @Override
     public Result onUnauthorized(Context ctx) {
         return redirect(routes.LoginController.renderLogin());
