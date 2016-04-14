bcc8b9a68ac0962c6b884f1fa8f0d12dbfdcacca
tourn <latzer.daniel@gmail.com>
2016-04-14 10:11:32 +0200

    Add ExerciseDetailIT WIP

---
+++ test/integration/ExerciseDetailIT.java
@@ -0,0 +1,22 @@
+package integration;
+
+import org.junit.Test;
+
+import static helper.RegexMatcher.matches;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by tourn on 11.4.16.
+ */
+public class ExerciseDetailIT extends AbstractIntegrationTest{
+    @Test
+    public void test() {
+        as(FRANZ, browser -> {
+            browser.goTo("/dashboard");
+            assertThat(browser.pageSource(), matches("An1I.*1/1"));
+            assertThat(browser.pageSource(), matches("SE2.*0/5"));
+
+            assertThat(browser.pageSource(), matches("HOW CAN YOU BE SO WRONG!.*Franz.*8000.*Basic Math"));
+        });
+    }
+}
