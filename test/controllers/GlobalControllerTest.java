50746e65f41f60484643abf32a2cc6f666f626b8
gummibjorn <marioegli95@gmail.com>
2016-04-15 12:59:58 +0200

    add test for globalcontroller

---
+++ test/controllers/GlobalControllerTest.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static play.test.Helpers.*;
+
+/**
+ * Created by mario on 15.04.16.
+ */
+public class GlobalControllerTest {
+    @Test
+    public void testUnhandledRoutesRedirectedToPageNotFound() {
+        running(testServer(3333, fakeApplication()), FIREFOX, browser -> {
+            browser.goTo("http://localhost:3333/DefinitlyNonExistingRoutROFLMAO");
+            assertThat(browser.pageSource(), containsString("Page not found"));
+        });
+    }
+
+
+
+}
