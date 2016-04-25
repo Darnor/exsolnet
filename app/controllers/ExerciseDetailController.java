fbd2a1315556de616529854f4d9bda5f5da261da
blubiszirp <frank-jordi@hotmail.com>
2016-04-25 14:47:21 +0200

    add Solutionsorting for SolutionListView

--- app/controllers/ExerciseDetailController.java
+++ app/controllers/ExerciseDetailController.java
@@ -15,6 +15,9 @@ import views.html.error404;
 import views.html.exerciseSolutions;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.List;
 
 @Security.Authenticated(Secured.class)
 public class ExerciseDetailController extends Controller {
@@ -62,8 +65,99 @@ public class ExerciseDetailController extends Controller {
      * @param exerciseId the id of the Exercise
      * @return Result view of the exercise with all solutions and comments.
      */
-    public Result renderSolutions(Long exerciseId) {
-        return ok(exerciseSolutions.render(SessionService.getCurrentUser(), Exercise.findById(exerciseId)));
+    public Result renderSolutions(Long exerciseId){
+        Exercise exercise = Exercise.findById(exerciseId);
+        return ok(exerciseSolutions.render(SessionService.getCurrentUser(), exercise, getSortedSolutionList(exercise.getSolutions())));
+    }
+
+
+
+    /**
+     * sorts (and copy) the solution-list of the exercise for the View.
+     * 1. Official Solution (or best solution if it has no official solution)
+     * 2. Best Solution (or second best if it has no official solution or it is the best solution)
+     * 3. Rest (sorted after date)
+     *
+     * @param solutions which should be sorted
+     * @return a sorted solution List (copy)
+     */
+    protected static List<Solution> getSortedSolutionList(List<Solution> solutions){
+        List<Solution> sortedSolutions = new ArrayList<>(solutions);
+        if(sortedSolutions.size()>2){
+            sortedSolutions = sortWithTwoOrMoreElements(sortedSolutions);
+        }else{
+            sortedSolutions.sort((e1,e2) -> (int)e1.getPoints()-(int)e2.getPoints());
+        }
+        return sortedSolutions;
+    }
+
+    /**
+     * Helper-Method for getSortedSolutionList.
+     * Sorts the Solution-list if it has more than 2 elements
+     *
+     * @param sortedSolutions which should be sorted
+     * @return a sorted solution List
+     */
+    private static List<Solution> sortWithTwoOrMoreElements(List<Solution> sortedSolutions){
+        Solution officialSolution = sortedSolutions.stream().filter(s -> s.isOfficial()).findFirst().orElse(null);
+        if(officialSolution != null){
+            sortedSolutions = sortWithOfficialSolution(sortedSolutions,officialSolution);
+        }else{
+            sortedSolutions = sortWithoutOfficialSolution(sortedSolutions);
+        }
+        sortedSolutions = moveWorseSolutionToTheEnd(sortedSolutions);
+        return sortedSolutions;
+    }
+
+    /**
+     * Helper-Method for getSortedSolutionList.
+     * Moves worse solution to the end of the list
+     * Worst solution will be the last element
+     *
+     * @param sortedSolutions which will be edited
+     * @return a sorted solution List
+     */
+    private static List<Solution> moveWorseSolutionToTheEnd(List<Solution> sortedSolutions){
+        int worsePointsLimit = -3;
+        List<Solution> worseSolution = sortedSolutions.stream().filter(s -> s.getPoints()<=worsePointsLimit).collect(Collectors.toList());
+        sortedSolutions.removeAll(worseSolution);
+        worseSolution.sort((e1,e2) -> (int)e2.getPoints()-(int)e1.getPoints());
+        worseSolution.stream().forEach(s -> sortedSolutions.add(s));
+        return sortedSolutions;
+    }
+
+    /**
+     * Helper-Method for getSortedSolutionList.
+     * Sorts the Solution-list if it has more than 2 elements and an official solution
+     *
+     * @param sortedSolutions which should be sorted
+     * @return a sorted solution List
+     */
+    private static List<Solution> sortWithOfficialSolution(List<Solution> sortedSolutions, Solution officialSolution){
+        sortedSolutions.remove(officialSolution);
+        sortedSolutions.sort((e1,e2) -> (int)e2.getPoints()-(int)e1.getPoints());
+        Solution bestSolution = sortedSolutions.remove(0);
+        sortedSolutions.sort((e1,e2) -> e2.getTime().compareTo(e1.getTime()));
+        sortedSolutions.add(0,bestSolution);
+        sortedSolutions.add(0,officialSolution);
+        return sortedSolutions;
+    }
+
+    /**
+     * Helper-Method for getSortedSolutionList.
+     * Sorts the Solution-list if it has more than 2 elements and no official solution
+     *
+     * @param sortedSolutions which should be sorted
+     * @return a sorted solution List
+     */
+    private static List<Solution> sortWithoutOfficialSolution(List<Solution> sortedSolutions){
+        sortedSolutions.sort((e1,e2) -> (int)e2.getPoints()-(int)e1.getPoints());
+        Solution bestSolution = sortedSolutions.remove(0);
+        Solution secondBestSolution = sortedSolutions.remove(0);
+        sortedSolutions.sort((e1,e2) -> e2.getTime().compareTo(e1.getTime()));
+        sortedSolutions.add(0,secondBestSolution);
+        sortedSolutions.add(0,bestSolution);
+        return sortedSolutions;
     }
 
     /**
---
+++ test/controllers/ExerciseDetailControllerTest.java
@@ -0,0 +1,27 @@
+package controllers;
+
+
+import models.AbstractModelTest;
+import models.Exercise;
+import models.Solution;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class ExerciseDetailControllerTest extends AbstractModelTest {
+
+    @Test
+    public void testSortSolutionsWithMoreThanTwoSolutionsAndOfficialSolution(){
+        List<Solution> sortedSolutions = ExerciseDetailController.getSortedSolutionList(Exercise.findById(8005L).getSolutions());
+
+        // First element should be official
+        assertTrue(sortedSolutions.get(0).isOfficial());
+
+        // Last element should be the worst
+        assertThat(sortedSolutions.get(sortedSolutions.size()-1).getContent(),containsString("www.iamhans.com"));
+    }
+}
