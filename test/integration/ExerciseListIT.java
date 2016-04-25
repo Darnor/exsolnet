6dc63c57ec41405ca493555e009c1767bb8a9a9b
Claudia Saxer <Claudia Saxer>
2016-04-25 17:37:14 +0200

    rewrite IT tests

--- test/integration/ExerciseListIT.java
+++ test/integration/ExerciseListIT.java
@@ -3,6 +3,7 @@ package integration;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import play.Logger;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,8 +17,10 @@ public class ExerciseListIT extends AbstractIntegrationTest{
     public void testNoFilterExercises() {
         as(FRANZ, browser -> {
             browser.goTo("/exercises");
+            Logger.info(browser.pageSource());
+            Logger.info(browser.pageSource());
             assertThat(browser.pageSource(), matches("Ableitung 1a"));
-            assertThat(browser.pageSource(), matches("von.*Blubberduck.*An2I.*Ableiten.*Funktion"));
+            assertThat(browser.pageSource(), matches("Ableiten.*An2I.*Funktion.*Blubberduck.*1.*2 Dez 2015"));
         });
     }
 
@@ -27,7 +30,7 @@ public class ExerciseListIT extends AbstractIntegrationTest{
             browser.click("a",withText("Aufgaben"));
             browser.await().atMost(1, TimeUnit.SECONDS).untilPage().isLoaded();
             assertThat(browser.pageSource(), matches("Ableitung 1a"));
-            assertThat(browser.pageSource(), matches("von.*Blubberduck.*An2I.*Ableiten.*Funktion"));
+            assertThat(browser.pageSource(), matches("Ableiten.*An2I.*Funktion.*Blubberduck"));
         });
     }
 
@@ -37,7 +40,7 @@ public class ExerciseListIT extends AbstractIntegrationTest{
             browser.goTo("/exercises");
             browser.fill("#title-filter").with("XXX");
             browser.submit("#submit-filter");
-            assertThat(browser.pageSource(), matches("XXX.*von.*Hans.*Allgemein"));
+            assertThat(browser.pageSource(), matches("XXX.*Allgemein.*Hans"));
         });
     }
 
@@ -48,7 +51,7 @@ public class ExerciseListIT extends AbstractIntegrationTest{
             WebElement mainTagElement = browser.getDriver().findElement(By.id("token-input-tag-filter-list"));
             fillTagTokenElements(browser, mainTagElement, "Allgemein");
             browser.submit("#submit-filter");
-            assertThat(browser.pageSource(), matches("XXX.*von.*Hans.*Allgemein"));
+            assertThat(browser.pageSource(), matches("XXX.*Allgemein.*Hans"));
         });
     }
 
@@ -60,7 +63,7 @@ public class ExerciseListIT extends AbstractIntegrationTest{
             WebElement mainTagElement = browser.getDriver().findElement(By.id("token-input-tag-filter-list"));
             fillTagTokenElements(browser, mainTagElement, "Allgemein");
             browser.submit("#submit-filter");
-            assertThat(browser.pageSource(), matches("XXX.*von.*Hans.*Allgemein"));
+            assertThat(browser.pageSource(), matches("XXX.*Allgemein.*Hans"));
         });
     }
 
@@ -70,7 +73,7 @@ public class ExerciseListIT extends AbstractIntegrationTest{
             browser.goTo("/exercises");
             browser.click("a",withText("Titel"));
             browser.await().atMost(1, TimeUnit.SECONDS).untilPage().isLoaded();
-            assertThat(browser.pageSource(), matches("XXX.*von.*Hans.*Allgemein"));
+            assertThat(browser.pageSource(), matches("XXX.*Allgemein.*Hans"));
         });
     }
 }
