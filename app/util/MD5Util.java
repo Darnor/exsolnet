8a768286b2a0d07267cd372167215153c5aff5d2
Hansruedi Patzen <hp@revy.ch>
2016-05-04 16:26:30 +0200

    happy md5util sonar

--- app/util/MD5Util.java
+++ app/util/MD5Util.java
@@ -1,28 +1,30 @@
 package util;
 
+import play.Logger;
+
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-/**
- * Created by tourn on 4.5.16.
- */
 public class MD5Util {
-    public static String hex(byte[] array) {
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < array.length; ++i) {
-            sb.append(Integer.toHexString((array[i]
-                    & 0xFF) | 0x100).substring(1,3));
+
+    private MD5Util() {}
+
+    private static String hex(byte[] array) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : array) {
+            sb.append(Integer.toHexString((b & 0xFF) | 0x100).substring(1,3));
         }
         return sb.toString();
     }
     public static String md5Hex (String message) {
         try {
-            MessageDigest md =
-                    MessageDigest.getInstance("MD5");
+            MessageDigest md = MessageDigest.getInstance("MD5");
             return hex (md.digest(message.getBytes("CP1252")));
         } catch (NoSuchAlgorithmException e) {
+            Logger.error("Exception " + e.getMessage());
         } catch (UnsupportedEncodingException e) {
+            Logger.error("Exception " + e.getMessage());
         }
         return null;
     }
