6d5867ff0860a70d40a341e382437f605abab7ef
blubiszirp <frank-jordi@hotmail.com>
2016-05-09 15:02:41 +0200

    refactor updateComment

--- app/controllers/CommentController.java
+++ app/controllers/CommentController.java
@@ -3,12 +3,14 @@ package controllers;
 import models.Comment;
 import models.Exercise;
 import models.Solution;
+import models.User;
 import play.Logger;
 import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
 import services.SessionService;
+import views.html.error404;
 
 import javax.inject.Inject;
 
@@ -33,17 +35,19 @@ public class CommentController extends Controller {
 
     public Result updateComment(Long commentId) {
         Comment comment = Comment.findById(commentId);
-        if(comment.getUser().getId().equals(SessionService.getCurrentUser().getId())){
+        User user = SessionService.getCurrentUser();
+        if(comment.getUser().getId().equals(user.getId())){
             Comment.updateContent(commentId, formFactory.form().bindFromRequest().get(CONTENT_FIELD));
             Logger.debug("Comment: "+commentId+" updated with content: "+formFactory.form().bindFromRequest().get(CONTENT_FIELD));
         }
         else{
-            Logger.error("User: "+SessionService.getCurrentUser().getId()+" tried to edit comment: "+commentId+" from user: "+comment.getUser().getId());
+            Logger.error("User: "+user.getId()+" tried to edit comment: "+commentId+" from user: "+comment.getUser().getId());
         }
-        if(comment.getExercise()!=null)
+        if(comment.getExercise()!=null) {
             return redirect(routes.ExerciseController.renderDetail(comment.getExercise().getId()));
-        if(comment.getSolution() != null)
+        }else if(comment.getSolution() != null) {
             return redirect(routes.ExerciseController.renderDetail(comment.getSolution().getExercise().getId()));
-        return null;
+        }
+        return notFound(error404.render(user,"Bearbeiteter Kommentar ist keiner Aufgabe oder Lösung zugewiesen"));
     }
 }
