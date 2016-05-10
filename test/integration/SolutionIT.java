a16bc2799522f4cc0dade4fe063ea5d194c0b17c
blubiszirp <frank-jordi@hotmail.com>
2016-05-10 10:22:32 +0200

    IT for create solution

--- test/integration/SolutionIT.java
+++ test/integration/SolutionIT.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static helper.RegexMatcher.matches;
 import static org.fluentlenium.core.filter.FilterConstructor.withId;
+import static org.fluentlenium.core.filter.FilterConstructor.withText;
 import static org.junit.Assert.assertThat;
 import static play.test.Helpers.FIREFOX;
 
@@ -23,6 +24,24 @@ public class SolutionIT extends AbstractIntegrationTest {
             browser.submit("#save");
 
             assertThat(browser.pageSource(), matches(content+"*.Zuletzt geändert:"));
+        });
+    }
+
+    @Test
+    public void testCreateSolution() {
+        as(FRANZ, FIREFOX, browser -> {
+            String content = "Dies ist ein Test.";
+            browser.goTo("/exercises/8007");
+
+            browser.click("a",withText("Aufgabe Lösen"));
+
+            browser.await().untilPage().isLoaded();
+
+            fillCKEditor(browser, content);
+
+            browser.submit("#save");
+
+            assertThat(browser.pageSource(), matches(content));
         });
     }
 }
